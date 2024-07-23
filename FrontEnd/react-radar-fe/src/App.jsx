898c8aa9eef9b67b706d707f1bff5e9b27889a38
Sunidhi <p.sunidhi3@gmail.com>
2024-07-23 22:55:31 +0530

    app

--- FrontEnd/react-radar-fe/src/App.jsx
+++ FrontEnd/react-radar-fe/src/App.jsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+// src/App.jsx
+// import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './components/Home';
-import Login from './components/Login';
-import Recommendations from './components/Recommendations';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RecommendationsPage from './pages/RecommendationsPage';
+import BookDetailsPage from './pages/BookDetailsPage';
+import ReadingListPage from './pages/ReadingListPage';
+import DiscussionForumPage from './pages/DiscussionForumPage';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/recommendations" component={Recommendations} />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/recommendations" element={<RecommendationsPage />} />
+        <Route path="/book/:id" element={<BookDetailsPage />} />
+        <Route path="/reading-list" element={<ReadingListPage />} />
+        <Route path="/discussion-forum" element={<DiscussionForumPage />} />
+      </Routes>
+    </Router>
   );
 };
 
 export default App;
+
 
