010ad8d2a91321afbbb27bea67476a813ea22324
Sunidhi <p.sunidhi3@gmail.com>
2024-08-06 11:45:50 +0530

    fixed path errors

--- FrontEnd/react-radar-fe/src/App.jsx
+++ FrontEnd/react-radar-fe/src/App.jsx
@@ -1,28 +1,28 @@
 // src/App.jsx
 // import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import RecommendationsPage from './pages/RecommendationsPage';
-import BookDetailsPage from './pages/BookDetailsPage';
-import ReadingListPage from './pages/ReadingListPage';
-import DiscussionForumPage from './pages/DiscussionForumPage';
+import Header from './components/Header'
+import Home from './components/Home'
+import Login from './components/Login';
+import Recommendations from './components/Recommendations';
+import BookDetails from './components/BookDetails';
+import ReadingList from './components/ReadingList'
+import DiscussionForum from './components/DiscussionForum';
 
 const App = () => {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/recommendations" element={<RecommendationsPage />} />
-        <Route path="/book/:id" element={<BookDetailsPage />} />
-        <Route path="/reading-list" element={<ReadingListPage />} />
-        <Route path="/discussion-forum" element={<DiscussionForumPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/reading-list" element={<ReadingList />} />
+        <Route path="/discussion-forum" element={<DiscussionForum />} />
       </Routes>
     </Router>
-  );
+ );
 };
 
 export default App;
